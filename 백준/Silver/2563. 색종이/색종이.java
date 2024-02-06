6f8644627dd8be354050ece6cda22336dfc23546
psy-er <89902211+psy-er@users.noreply.github.com>
2024-02-06 17:29:50 +0900

    [Silver V] Title: 색종이, Time: 120 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int total = 0;  // 검은 영역의 넓이
+        int n = Integer.parseInt(br.readLine());  // 색종이 개수
+        
+        boolean[][] arr = new boolean[101][101];  // 도화지
+        
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            
+            // 색종이는 가로 세로의 크기가 10이다
+            // (x,y)부터 (x+9, y+9)까지의 영역을 하나씩 체크한 후 총 넓이에 더해준다
+            
+            for (int j = x; j < x+10; j++) { // 색종이 하나씩 생각해주기
+                for (int k = y; k < y+10; k++) {
+                    if (!arr[j][k]) { // 0은 false, 양수는 true
+                        arr[j][k] = true;
+                        total++;
+                    }
+                }
+            }
+        }
+        System.out.print(total);
+    }
+}
